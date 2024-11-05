452b9c5e1be625ff7a8183d960b1bb3a1ba771c5
Ericriera <ericrp2002@gmail.com>
2024-11-05 19:40:12 +0100

    fix: cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import questions, suggestions
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(questions.router)
 app.include_router(suggestions.router)
