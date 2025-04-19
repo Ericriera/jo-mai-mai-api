ad5fa28456d58277b013b5c738268e2d8d3e9458
Ericriera <ericrp2002@gmail.com>
2025-04-19 17:46:19 +0200

    Revert "fix: keys in env for deploy"
    This reverts commit 08d4ecabe981f8b09536859d1d4d6be997882fbc.

--- db/client.py
+++ db/client.py
@@ -1,13 +1,3 @@
-import os
-import json
 from google.cloud import firestore
-from google.oauth2 import service_account
-from dotenv import load_dotenv
 
-load_dotenv()
-
-SERVICE_ACCOUNT_KEY = json.loads(os.getenv("SERVICE_ACCOUNT_KEY"))
-
-credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_KEY)
-
-db_client = firestore.Client(credentials=credentials)
+db_client = firestore.Client.from_service_account_json("./serviceAccountKey.json")
