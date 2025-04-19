08d4ecabe981f8b09536859d1d4d6be997882fbc
Ericriera <ericrp2002@gmail.com>
2025-04-19 17:04:57 +0200

    fix: keys in env for deploy

--- db/client.py
+++ db/client.py
@@ -1,3 +1,13 @@
+import os
+import json
 from google.cloud import firestore
+from google.oauth2 import service_account
+from dotenv import load_dotenv
 
-db_client = firestore.Client.from_service_account_json("./serviceAccountKey.json")
+load_dotenv()
+
+SERVICE_ACCOUNT_KEY = json.loads(os.getenv("SERVICE_ACCOUNT_KEY"))
+
+credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_KEY)
+
+db_client = firestore.Client(credentials=credentials)
